8f11430deb2d0f04f89571efeb177c101af6cec5
Timur Badretdinov <accounts@destiner.io>
2024-04-08 15:11:54 +0100

    Set up CORS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
 import { label } from '@/routes/index.js';
 
 const app = new Hono();
 
+app.use(cors());
 app.get('/', (c) => {
   return c.text('OK');
 });
