ccea40f4cac98b4f05a786b9728e5ad7af1a056b
Timur Badretdinov <accounts@destiner.io>
2024-05-12 03:52:53 +0100

    Add contract operations to Minio service

--- src/services/minio.ts
+++ src/services/minio.ts
@@ -1,9 +1,28 @@
 import { Readable } from 'node:stream';
 
 import * as Minio from 'minio';
+import { Abi, Address } from 'viem';
 
 import { ChainId } from '@/utils/chains';
 import { ChainLabelMap } from '@/utils/labels';
+import { SourceCode } from '@/utils/sources';
+
+interface BaseContract {
+  source: SourceCode;
+  abi: Abi;
+  isProxy: boolean;
+}
+
+interface StaticContract extends BaseContract {
+  isProxy: false;
+}
+
+interface ProxyContract extends BaseContract {
+  isProxy: true;
+  implementation: Address | null;
+}
+
+type Contract = StaticContract | ProxyContract;
 
 async function streamToString(stream: Readable): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -52,6 +71,30 @@ class Service {
       return {};
     }
   }
+
+  async getContract(
+    chain: ChainId,
+    address: Address,
+  ): Promise<Contract | null> {
+    const key = `contracts/${chain}/${address}.json`;
+    try {
+      const file = await this.client.getObject(this.bucket, key);
+      const fileString = await streamToString(file);
+      return JSON.parse(fileString);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async setContract(
+    chain: ChainId,
+    address: Address,
+    code: Contract,
+  ): Promise<void> {
+    const key = `contracts/${chain}/${address}.json`;
+    await this.client.putObject(this.bucket, key, JSON.stringify(code));
+  }
 }
 
 export default Service;
+export type { BaseContract };
